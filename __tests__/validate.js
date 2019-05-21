f15fc06f38bea36665bd03f00c5d843160d086d5
Alf Eaton <eaton.alf@gmail.com>
2019-05-21 19:52:53 +0100

    Load DTD for validation tests

--- __tests__/validate.js
+++ __tests__/validate.js
@@ -1,5 +1,26 @@
+const fs = require('fs')
+const libxml = require('libxmljs')
 const validator = require('schematron-runner')
 
-module.exports = name => validator.validate(
-  `${__dirname}/../testfiles/${name}`,
-  `${__dirname}/../schematrons/1.0/jats4r.sch`)
+// process.env.XML_DEBUG_CATALOG = true
+// process.env.XML_CATALOG_FILES = require.resolve('@jats4r/dtds/schema/catalog.xml')
+
+module.exports = name => {
+  const xml = fs.readFileSync(`${__dirname}/../testfiles/${name}`, 'utf8')
+
+  const doc = libxml.parseXmlString(xml, {
+    dtdattr: true,
+    dtdload: true,
+    dtdvalid: true,
+    nonet: true,
+  })
+
+  if (doc.errors.length) {
+    throw new Error(doc.errors.map(error => error.message).join('\n'))
+  }
+
+  return validator.validate(
+    doc.toString(),
+    `${__dirname}/../schematrons/1.0/jats4r.sch`
+  )
+}
